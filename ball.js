e64e94e82e341cdbaa99e7fce47a5dd28c9bfb32
deveshgoyal <me@deveshgoyal.com>
2018-04-11 16:37:39 +0530

    game basic

--- ball.js
+++ ball.js
@@ -1,66 +1,179 @@
 var SPEED = 3;
 
+
 var Ball = function () {
-    this.r = 5;
+
+    this.mass = random(2, 2)
+    this.r = this.mass * 15;
     this.color = color(150, 134, 200, 100)
     this.strokeColor = color(150, 134, 200, 200)
-    this.position = createVector(random(0 + 20, width - 20), random(0 + 20, height - 20));
-    this.velocity = createVector(random(-1 * SPEED, SPEED), random(-1 * SPEED, SPEED));
-    this.default_velocity = this.velocity
+    this.position = createVector(200, height / 2)  //createVector(random(0 + 20, width - 20), random(0 + 20, height - 20));
+    this.velocity = createVector(0, 0) //createVector(random(-1 * SPEED, SPEED), random(-1 * SPEED, SPEED));
+    this.acceleration = createVector(0, 0)
+    this.default_velocity = this.velocity;
+    var GRAVITY = createVector(0, 0.3)
 
 
     //cavvas edge
     this.detectCanvasEdge = function () {
-        if (this.position.x < 0 + this.r || this.position.x > width - this.r) {
+
+        if (this.position.x < 0 + this.r) {
             this.velocity.x = this.velocity.x * (-1);
-            this.color = color(150, 134, 200, 100)
-        }
-        if (this.position.y < 0 + this.r || this.position.y > height - this.r) {
-            this.velocity.y = this.velocity.y * (-1);
-            this.color = color(150, 134, 200, 100)
+            this.applyForce(createVector(0, -0.20))
         }
 
-        this.position.add(this.velocity);
+        if (this.position.x > width - this.r) {
+            this.velocity.x = this.velocity.x * (-1);
+            this.position.x = width - this.r;
+        }
+
+        if (this.position.y < 0 - this.r || this.position.y > height - this.r) {
+            this.velocity.y = this.velocity.y * (-1);
+            this.position.y = height - this.r;
+            // noLoop();
+        }
+
     }
 
 
 
+    this.hit = function (other) {
+
+        return (this.position.x + this.r > other.position1.x &&
+            this.position.y + this.r > other.position1.y &&
+            this.position.x - this.r < other.position1.x + other.w &&
+            this.position.y - this.r < other.position1.y + other.h1)
+
+            || (this.position.x + this.r > other.position.x &&
+                this.position.y + this.r > other.position.y &&
+                this.position.x - this.r < other.position.x + other.w &&
+                this.position.y - this.r < other.position.y + other.h)
+
+    }
 
     //update
     this.update = function () {
 
-        var target = createVector(mouseX, mouseY)
-        var desired = p5.Vector.sub(target, this.position);
-        desired.normalize();
+        this.applyGravity()
+        // this.applyForce(createVector(0.02,0))
+        // this.applyForce(createVector(0.60,0))
 
 
-        // var steer = p5.Vector.sub(desired, this.velocity);
-        // steer.mult(1)
-        // this.velocity = steer;
-        // this.velocity = steer;
 
-        var mouseRadius = 40;
 
-        if ( mouseRadius > dist(this.position.x, this.position.y, mouseX, mouseY) ) {
-            // near mouse radious
-            this.color.setAlpha(map(dist(this.position.x, this.position.y, mouseX, mouseY), 0, mouseRadius, 255, 0))
-            this.velocity = desired.mult(-1)
-        } else {
-            this.color.setAlpha(0);
-            this.velocity = this.default_velocity
-        }
-
+        this.acceleration.limit(5);
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
 
         this.detectCanvasEdge()
         this.draw();
+        this.acceleration.mult(0)
+        // this.position.limit(height);
     }
 
     //draw
     this.draw = function () {
         fill(this.color)
         stroke(this.strokeColor);
-        ellipse(this.position.x, this.position.y, this.r * 2, this.r * 2)
+        // ellipse(this.position.x, this.position.y, this.r * 2, this.r * 2)
+
+        image(window.birdImg, this.position.x - this.r , this.position.y - this.r, this.r * 2, this.r * 2)
+        // window.birdImg.position(this.position.x - this.r , this.position.y - this.r)//,this.r * 2, this.r * 2)
+        // window.birdImg.size(this.r * 2, this.r * 2);
+
     }
+
+
+    this.random_mouse = function () {
+        // var target = createVector(mouseX, mouseY)
+        // var desired = p5.Vector.sub(target, this.position);
+        // desired.normalize();
+
+
+        // var steer = p5.Vector.sub(desired, this.velocity);
+        // steer.mult(1)
+        // // this.velocity = steer;
+        // // this.velocity = steer;
+
+        // var mouseRadius = 400;
+
+        // if ( mouseRadius > dist(this.position.x, this.position.y, mouseX, mouseY) && !this.flag && mouseIsPressed ) {
+        //     // near mouse radious
+        //     this.color.setAlpha(map(dist(this.position.x, this.position.y, mouseX, mouseY), 0, mouseRadius, 255, 0))
+        //     this.velocity = steer.mult(-1) //desired.mult(-1)
+        //     this.flag = false
+        // }// else {
+        //     // this.color.setAlpha(0);
+        //     // this.velocity = this.default_velocity
+        // // }
+    }
+
+    //gravity
+    this.applyGravity = function () {
+        if (typeof GRAVITY !== 'undefined')
+            this.acceleration.add(GRAVITY);
+    }
+
+    this.applyForce = function (f) {
+        var force = p5.Vector.div(f, this.mass)
+        this.acceleration.add(force);
+
+
+    }
+
+
+
+}
+
+
+var Wall = function (x, y) {
+
+    this.position = createVector(width, 0);
+
+
+
+    this.velocity = createVector(-10, 0)
+
+    this.w = 50;
+    this.h = 200;
+    this.h1 = 200;
+
+    this.reset = function () {
+        this.position.x = width;
+        this.position1.x = width
+        this.w = 50;
+        this.h = random(100, 300)
+        this.position1.y = 150 + this.h;
+        this.h1 = height - 150 - this.h
+    }
+
+    this.position1 = createVector(width, 150 + this.h);
+
+
+    this.reset();
+
+
+    this.update = function () {
+
+        this.position.add(this.velocity)
+        this.position1.add(this.velocity)
+
+
+        if (this.position.x + this.w < 0) {
+            this.reset();
+        }
+
+        this.show()
+
+    }
+
+    this.show = function () {
+        fill(0);
+        rect(this.position.x, this.position.y, this.w, this.h);
+        rect(this.position1.x, this.position1.y, this.w, this.h1);
+
+    }
+
 
 
 }
--- sketch.js
+++ sketch.js
@@ -1,21 +1,124 @@
 
 var balls = [];
+var walls;
+window.count = 0
+window.birdImg;
 // Remember how Processing works? setup() is executed once when the sketch starts and draw() loops forever and ever (until you quit).
+function preload() {
+  birdImg = loadGif('bird.gif');
+  bg = loadImage("bg.png")
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(25);
 
-  for (var i = 0; i < 2000; i++) {
+
+  wall = new Wall(mouseX, mouseY)
+
+  for (var i = 0; i < 1; i++) {
     balls.push(new Ball())
-    
   }
 
+d = createVector(0,0)
+
 }
+
+
 
 function draw() {
-  background(25);
+  // frameRate(10)
+  background(200);
   for (var i = 0; i < balls.length; i++) {
     var ball = balls[i];
+
+    image(bg, d.x , d.y, width * 1920 / 558  , height  )
+    d.x = d.x - 8
+    if((width * 1920 / 558) + d.x < width){
+      d.x = 0
+    }
+
+    var c = 0.1;
+    var friction = ball.velocity.copy();
+    friction.mult(-1);
+    friction.normalize();
+    friction.mult(c);
+
+    ball.applyForce(friction)
+    // ball.applyForce(createVector(0.02,0))
+
+
+
+    // if (keyIsDown(LEFT_ARROW)) {
+    //   // ball.applyForce(createVector(-1, 0))
+    // } else if (keyIsDown(RIGHT_ARROW)) {
+    //   // ball.applyForce(createVector(1, 0))
+    // } else if (keyIsDown(UP_ARROW)) {
+
+    //   ball.applyForce(createVector(0, -6))
+    // } else if (keyIsDown(DOWN_ARROW)) {crore
+    //   // ball.applyForce(createVector(0, 1))
+    // }
+
+
+    ball.r = ball.r;
     ball.update()
+
+    
+
+    
+      // window.birdImg.pause()
+
+      // window.birdImg.filter("threshold", 1);
+
+    textSize(50)
+    text("Scrre : " + window.count, width / 2 - 300, 40)
+
+    if ( false && ball.hit(wall)) {
+      ball.color.setAlpha(0)
+      noLoop()
+      //background(255)
+      window.birdImg.remove()
+      fill(0)
+      textSize(40)
+      image(bg, 0, 0,width,height)
+
+      text("Game Over. Score : " + window.count, width / 2.3, height / 2)
+      return false;
+    } else {
+      if (wall.position.x < 0) {
+        window.count++;
+      }
+      ball.color.setAlpha(100)
+    }
+
+    wall.update();
+
+
+
   }
+
+
+  //  ellipse(mouseX, mouseY, 20)
+
+
+
+
 }
+
+function keyPressed() {
+
+  if (keyCode === UP_ARROW) {
+    balls[0].applyForce(createVector(0, -30))
+
+    if(!window.birdImg.playing())
+    window.birdImg.play()
+    
+    setTimeout(function () {  
+      // window.birdImg.pause()  
+    },1000)
+
+  }
+
+}
+
